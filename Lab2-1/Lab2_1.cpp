0e0d5d657d75aae001574ca21bf0ce2e63ee6d15
Huzaifah Tahir <tahirhuzaifah@outlook.com>
2018-09-22 15:15:40 -0400

    Transfer

---
+++ Lab2-1/Lab2_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num1, num2, total;   // Define 3 integers
+
+	num1 = 3;
+
+	num2 = 4;
+
+	total = num1 + num2;
+
+	cout << "The total  is " << total;
+
+	return 0;
+
+}
